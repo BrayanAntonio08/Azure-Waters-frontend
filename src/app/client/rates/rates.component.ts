7c2ee2b4c4624320b463ad8ee96078464cefcaa0
ErickaMeneses <erims29.4@gmail.com>
2024-04-13 14:42:27 -0600

    update

--- src/app/client/rates/rates.component.ts
+++ src/app/client/rates/rates.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rates',
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class RatesComponent {
 
+}*/
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rates',
+  standalone: true,
+  imports: [],
+  templateUrl: './rates.component.html',
+  styleUrls: ['./rates.component.css']
+})
+
+export class RatesComponent {
+  roomTypes = [
+    { name: 'Single', lowSeasonPrice: 100, highSeasonPrice: 150, imageUrl: 'assets/bedroom.jpg' },
+    { name: 'Double', lowSeasonPrice: 150, highSeasonPrice: 200 },
+  ];
 }
+
+
