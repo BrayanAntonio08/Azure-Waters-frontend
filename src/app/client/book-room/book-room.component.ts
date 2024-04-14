b5bf54a08ca8bd449185ae6e0591531a1b615b46
BrayanAntonio08 <brayanvafer@gmail.com>
2024-04-13 18:54:47 -0600

    finalización de página de reserva

--- src/app/client/book-room/book-room.component.ts
+++ src/app/client/book-room/book-room.component.ts
@@ -11,27 +11,40 @@ import { Client } from '../../core/models/Client';
 })
 export class BookRoomComponent {
 
+  // variables de datos
   estado: string = "";
-  arriving? :Date;
-  departing? : Date;
-  codigoReserva = "";
+  room = {
+    img: "https://www.culmia.com/blog/wp-content/uploads/2021/07/Como-distribuir-los-muebles-en-una-habitacion.jpg",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos in magni velit cum tempora amet soluta tempore nostrum excepturi accusantium iure laudantium corrupti dolorum beatae ipsam, repellendus iusto fugiat rerum?"
+  }
   cliente: Client = new Client();
 
-  buscarHabitacion(){
+  // variables de entrada para la transacción
+  arriving?: Date;
+  departing?: Date;
+  codigoReserva = "";
+  
+
+  buscarHabitacion() {
     console.log(this.arriving);
     console.log(this.departing);
-    this.estado = "booking";
+
+    let found = Math.random() < 0.5; // implementación provisional de si hay disponibilidad o no
+    if (found)
+      this.estado = "booking";
+    else 
+      this.estado = "failed";
   }
 
-  crearReservacion(){
+  crearReservacion() {
     this.codigoReserva = this.newGuid();
     this.estado = "success";
   }
 
   newGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
       var r = Math.random() * 16 | 0,
-      v = c == 'x' ? r : (r & 0x3 | 0x8);
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
