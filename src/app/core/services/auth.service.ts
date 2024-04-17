2fed75b604481d850fa326ab87184d21230b2cbc
83gerson <gersoncalvo83@gmail.com>
2024-04-17 13:18:40 -0600

    Ajustes Login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Login } from '../../models/login';
+import { Login } from '../models/login';
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +13,23 @@ export class AuthService {
 
   constructor(private http: HttpClient) {
     this.url = "http://localhost:7119/api/Usuarios";
-   }
+  }
 
   login(data: any): Observable<Login> {
     console.log(data);
     return this.http.post<Login>("http://localhost:7119/api/Usuarios/buscarUsuario?usuario=" + data.usuario + "&contrasenna=" + data.contrasenna, data);
   }
 
-
   isAdminActive(): boolean {
-    this.http.get(this.url).subscribe(data=> {
+    this.http.get(this.url).subscribe(data => {
       console.log(data);
     })
     return this.active;
+  }
+
+  logout(): void {
+    sessionStorage.removeItem('id');
+    sessionStorage.removeItem('usuario');
+    sessionStorage.removeItem('contrasenna');
   }
 }
