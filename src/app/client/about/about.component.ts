78846acb3abfbc7ce328c76fecbbeeb6c34e901c
BrayanAntonio08 <brayanvafer@gmail.com>
2024-04-28 11:13:29 -0600

    Cargar imagenes en sobre nosotros y tarifas

--- src/app/client/about/about.component.ts
+++ src/app/client/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PageService } from '../../core/services/page.service';
 import { Page } from '../../core/models/Page';
+import { Image } from '../../core/models/Image';
 
 @Component({
   selector: 'app-about',
@@ -9,37 +10,24 @@ import { Page } from '../../core/models/Page';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent implements OnInit{
+export class AboutComponent {
 
-  images: string[] = [];
-  activeImage : string = "";
+  images: Image[] = [];
+  activeImage : Image = new Image();
   pageData : Page = new Page();
 
   constructor(pageService : PageService){
 
     pageService.loadPage("about").then((value) => this.pageData = value);
-  }
-
-  ngOnInit(): void {
     //Cargar las imagenes
-    this.images = [
-      "https://images.hola.com/imagenes/decoracion/20230425230358/dormitorios-inspirados-en-habitaciones-hoteles-am/1-237-28/habitaciones-hotel-5a-a.jpg",
-      "https://d3h1lg3ksw6i6b.cloudfront.net/media/image/2021/11/03/20193ba71b7249a697bdfcca3779e61f_hero_image_spain.jpg",
-      "https://cadenaser.com/resizer/nxbawbAS0xUJhkfbywRwyhui_Kc=/650x488/filters:quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/H73WX3XTQJIR7AAM2ZK6FMXAHQ.jpg",
-      "https://cdn.observador.cr/wp-content/uploads/2022/09/Casa-Turire-2.jpeg",
-      "https://revistasumma.com/wp-content/uploads/2021/01/hotel-.jpg",
-      "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_600,q_auto,w_600//hotelier-images/f5/c4/34b561f5b55be3d66ab43d2a0b012c735ec2d17b7ea039b67235afc707c6.jpeg",
-      "https://apetitoenlinea.com/wp-content/uploads/2020/11/Balmoral-2020-015-scaled-e1606228532946.jpg",
-      "https://conocedores.com/wp-content/uploads/2022/05/hoteles-all-inclusive-costa-rica-the-reatreat-spa-piscina-16052022.webp",
-      "https://www.eroomsuite.com/wp-content/uploads/2024/01/que-servicios-ofrece-un-hotel.webp",
-      "https://static.leonardo-hotels.com/image/leonardohotelbucharestcitycenter_room_comfortdouble2_2022_4000x2600_7e18f254bc75491965d36cc312e8111f_1200x780_mobile_3.jpeg",
-      "https://assets.tivolihotels.com/image/upload/q_auto,f_auto%2Cc_limit%2Cw_1378/media/minor/tivoli/images/brand_level/footer/1920x1000/thr_aboutus1_1920x1000.jpg",
-    ];
-    //activar la primer imagen
-    this.activeImage = this.images[0];
+    pageService.loadImages("about").then((value) => {
+      this.images = value;
+      //activar la primer imagen
+      this.activeImage = this.images[0];
+    });
   }
 
-  clickImage(img:string){
+  clickImage(img:Image){
     this.activeImage = img;
   }
 }
--- src/app/client/rates/rates.component.ts
+++ src/app/client/rates/rates.component.ts
@@ -1,18 +1,7 @@
-/*import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-rates',
-  standalone: true,
-  imports: [],
-  templateUrl: './rates.component.html',
-  styleUrl: './rates.component.css'
-})
-export class RatesComponent {
-
-}*/
-
 
 import { Component } from '@angular/core';
+import { RoomType } from '../../core/models/RoomType';
+import { RoomService } from '../../core/services/room.service';
 
 @Component({
   selector: 'app-rates',
@@ -21,13 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './rates.component.html',
   styleUrls: ['./rates.component.css']
 })
-
 export class RatesComponent {
-  roomTypes = [
-    { name: 'Habitación estándar', lowSeasonPrice: 140, highSeasonPrice: 180 , imageUrl: 'https://th.bing.com/th/id/R.74e5231e13476fc61e51ce34492491bf?rik=bSnynNm6B21n9A&riu=http%3a%2f%2fwww.magnosuites.com%2fwp-content%2fuploads%2f2016%2f06%2fhabitacion-estandar-magno-suites-1.jpg&ehk=fPW1%2bA5r74otLF%2f01a2iK0k4acPYyRhLP9vqAOLSnfg%3d&risl=&pid=ImgRaw&r=0'},
-    { name: 'Habitación familiar', lowSeasonPrice: 250, highSeasonPrice: 300 , imageUrl: 'https://hotelsuamox.com/wp-content/uploads/2020/04/5.HABITACI%C3%93N-FAMILIAR-con-ba%C3%B1o-privado-scaled.jpg'},
-    { name: 'Habitación de lujo', lowSeasonPrice: 380, highSeasonPrice: 420 , imageUrl: 'https://i.pinimg.com/originals/74/6d/92/746d92ea9f29f8a43eab60e57bc8ebda.jpg'}
-  ];
+  roomTypes : RoomType[] = [];
+  
+  constructor(private roomService: RoomService){
+    roomService.ListRoomTypes().then((value)=>{
+      this.roomTypes = value;
+    }).catch((error)=>{
+      console.log(error);
+    });
+  }
 }
 
 
