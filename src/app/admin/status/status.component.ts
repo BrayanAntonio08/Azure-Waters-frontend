5d3bc7699ae6f9ea5efd101f9229f06b88347992
ErickaMeneses <erims29.4@gmail.com>
2024-05-14 19:19:51 -0600

    US21
    Imprimir estado de habitación

--- src/app/admin/status/status.component.ts
+++ src/app/admin/status/status.component.ts
@@ -4,6 +4,10 @@ import { RoomService } from '../../core/services/room.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgIf } from '@angular/common';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import { RoomType } from '../../core/models/RoomType';
+
 
 @Component({
   selector: 'app-status',
@@ -14,7 +18,7 @@ import { NgIf } from '@angular/common';
 })
 export class StatusComponent {
 
-  habitacionesPorPagina: number = 6;
+  habitacionesPorPagina: number = 23;
   paginaActual: number = 1;
   fechaActual: Date = new Date();
 
@@ -25,6 +29,7 @@ export class StatusComponent {
   `;
 
   rooms: Room[] = [];
+  pdfPreviewUrl: string | null = null;
 
   constructor(private roomService: RoomService) { }
 
@@ -64,4 +69,57 @@ export class StatusComponent {
     }
   }
 
+  //NEW
+  printRoomStatus2(): void {
+    const doc = new jsPDF();
+    let y = 10;
+    this.rooms.forEach((room, index) => {
+      if (index % this.habitacionesPorPagina === 0 && index !== 0) {
+        doc.addPage();
+        y = 10;
+      }
+      doc.text(`Habitación ${room.id}: ${this.getRoomType(room)} - ${this.getRoomStatus(room)}`, 10, y);
+      y += 10;
+    });
+    doc.save('estado_habitaciones.pdf');
+  }
+
+  printRoomStatus(): void {
+    const doc = new jsPDF();
+    let y = 10;
+  
+    // Agregar título
+    doc.setFontSize(16);
+    doc.text('Azure Waters', 10, y);
+    y += 10;
+  
+    // Agregar subtítulo
+    doc.setFontSize(12);
+    doc.text('Estado de las habitaciones del Hotel el día de hoy', 10, y);
+    y += 10;
+  
+    // Agregar fecha actual
+    doc.setFontSize(10);
+    doc.text(`Fecha: ${this.fechaActual.toLocaleDateString()}`, 10, y);
+    y += 10;
+
+    // Agregar línea divisoria
+    doc.setLineWidth(0.5);
+    doc.line(10, y, 200, y); // Línea horizontal
+    y += 5; // Ajuste de espacio después de la línea divisoria
+
+  
+    // Agregar estado de las habitaciones
+    this.rooms.forEach((room, index) => {
+      if (index % this.habitacionesPorPagina === 0 && index !== 0) {
+        doc.addPage();
+        y = 10;
+      }
+      doc.text(`Habitación ${room.id}                                ${this.getRoomType(room)}                                ${this.getRoomStatus(room)}`, 10, y);
+      y += 10;
+    });
+    doc.save('estado_habitaciones.pdf');
+  }
+
+  
 }
