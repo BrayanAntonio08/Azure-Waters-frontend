8fd3278e826110a3fecb8412698e08b1a3e4cc76
ErickaMeneses <erims29.4@gmail.com>
2024-05-30 22:36:33 -0600

    Home admin

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild , ElementRef } from '@angular/core';
+import { Page } from '../../core/models/Page';
+import { PageService } from '../../core/services/page.service';
+import { Image } from '../../core/models/Image';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  pageData: Page = new Page();
+  image: Image = new Image();
+  searchTerm: string = '';
 
+  constructor(private pageService: PageService) { }
+
+  ngOnInit(): void {
+    this.loadPageData();
+    this.loadImages();
+  }
+
+  private loadPageData(): void {
+    this.pageService.loadPage('homeAdm').then((value) => this.pageData = value);
+  }
+
+  private loadImages(): void {
+    this.pageService.loadImages('homeAdm').then((value) => {
+      if (value.length > 0) {
+        this.image = value[0];
+      }
+    });
+  }
 }
