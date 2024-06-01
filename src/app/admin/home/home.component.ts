eae2165fc85aedd8826a99ff7057fee0263fe64e
ErickaMeneses <erims29.4@gmail.com>
2024-06-01 06:37:22 -0600

    modified home admin

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,41 +1,14 @@
-import { Component,ViewChild , ElementRef } from '@angular/core';
-import { Page } from '../../core/models/Page';
-import { PageService } from '../../core/services/page.service';
-import { Image } from '../../core/models/Image';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { NgIf } from '@angular/common';
+import { Component} from '@angular/core';
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, NgIf, CommonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
-  pageData: Page = new Page();
-  image: Image = new Image();
-  searchTerm: string = '';
 
-  constructor(private pageService: PageService) { }
-
-  ngOnInit(): void {
-    this.loadPageData();
-    this.loadImages();
-  }
-
-  private loadPageData(): void {
-    this.pageService.loadPage('homeAdm').then((value) => this.pageData = value);
-  }
-
-  private loadImages(): void {
-    this.pageService.loadImages('homeAdm').then((value) => {
-      if (value.length > 0) {
-        this.image = value[0];
-      }
-    });
-  }
 }
