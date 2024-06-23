c291fc211bb791d32c48da91c920870dea03c3a1
ErickaMeneses <erims29.4@gmail.com>
2024-06-23 16:10:25 -0600

    room

--- src/app/admin/room-availability/room-availability.component.ts
+++ src/app/admin/room-availability/room-availability.component.ts
@@ -1,84 +1,183 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room } from '../../core/models/Room';
 import { RoomType } from '../../core/models/RoomType';
 import { RoomService } from '../../core/services/room.service';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { NgIf } from '@angular/common';
-
-
 
 @Component({
-  selector: 'app-room-availability',
+  selector: 'app-disponibilidad-habitaciones',
   standalone: true,
-  imports: [FormsModule, NgIf, CommonModule],
+  imports: [CommonModule],
   templateUrl: './room-availability.component.html',
-  styleUrl: './room-availability.component.css'
+  styleUrls: ['./room-availability.component.css']
 })
-export class RoomAvailabilityComponent {
+export class RoomAvailabilityComponent implements OnInit {
+  habitaciones: Room[] = [];
+  listaTiposHabitacion: RoomType[] = [];
+  mensaje: string = '';
+  esError: boolean = false;
+  fechaLlegada: string = '';
+  fechaSalida: string = '';
+  tipoHabitacion: number = 0;
+  nombreTiposHabitacionMap: any;
+  precioTiposHabitacionMap: any;
+  tablaVisible: boolean = false;
 
-  habitacionesDisponibles: Room[] = []; // Array to store available rooms
-  fechaInicio: Date = new Date(); // Default start date
-  fechaFinal: Date = new Date(this.fechaInicio.getTime() + 24 * 60 * 60 * 1000); // Default end date (1 day after start date)
-  cargando: boolean = false; // Flag for loading state
-  tiposHabitaciones: RoomType[] = []; // Array to store room types
-  tipoHabitacionSeleccionado: RoomType | null = null; // Selected room type (optional)
-  tipoHabitacionSeleccionado2 = new RoomType();
-  costoEstancia: number = 0; // Static cost of stay
+  habitacionesPorPagina: number = 6;
+  paginaActual: number = 1;
 
-  constructor(private servicioHabitacion: RoomService) {} // Inject RoomService
+  constructor(private habitacionService: RoomService) { }
 
   ngOnInit(): void {
-    this.obtenerHabitacionesDisponibles();
-    this.obtenerTiposHabitacion(); // Fetch room types on component initialization
+    this.obtenerTiposHabitacion();
   }
 
-
-
-  obtenerHabitacionesDisponibles(): void {
-    this.cargando = true; // Establecer bandera de carga en verdadero
-    this.servicioHabitacion.getRoom() // Utilizar el método getRoom de RoomService
-      .subscribe(todasLasHabitaciones => {
-        // Filtrar habitaciones según los criterios de disponibilidad (asumiendo propiedades 'active' y 'reserved')
-        this.habitacionesDisponibles = todasLasHabitaciones.filter(habitacion => habitacion.active && !habitacion.reserved);
-  
-        // Aplicar filtro de tipo de habitación solo si se selecciona un tipo
-        if (this.tipoHabitacionSeleccionado) {
-          this.habitacionesDisponibles = this.habitacionesDisponibles.filter(habitacion => habitacion.type_id === this.tipoHabitacionSeleccionado2.id);
-
-        }
-  
-        this.cargando = false; // Establecer bandera de carga en falso después del filtrado
-      });
-  }
-
-  obtenerTiposHabitacion(): void {
-    this.servicioHabitacion.ListRoomTypes() 
-      .then(tipos => {
-        this.tiposHabitaciones = tipos;
-      });
-  }
-
-  consultarDisponibilidad(): void {
-    this.obtenerHabitacionesDisponibles(); 
-  }
-
-  seleccionarTipoHabitacion(tipo: RoomType): void {
-    this.tipoHabitacionSeleccionado = tipo;
-  }
-
-  getRoomType(room: Room): string {
-    switch (room.type_id) {
-      case 1:
-        return 'Estándar';
-      case 2:
-        return 'Familiar';
-      case 3:
-        return 'De lujo';
-      default:
-        return 'Tipo desconocido';
+  onInputChange(field: 'fechaLlegada' | 'fechaSalida' | 'tipoHabitacion', value: string | number) {
+    if (field === 'fechaLlegada') {
+      this.fechaLlegada = value as string;
+    } else if (field === 'fechaSalida') {
+      this.fechaSalida = value as string;
+    } else if (field === 'tipoHabitacion') {
+      this.tipoHabitacion = value as number;
     }
   }
+  obtenerNombreTipoHabitacion(idTipo: number): string {
+    return this.nombreTiposHabitacionMap[idTipo] || '';
+}
 
+obtenerPrecioTipoHabitacion(idTipo: number): string {
+    return this.precioTiposHabitacionMap[idTipo] || '';
+}
 
+  obtenerFechaActual(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = '' + (today.getMonth() + 1);
+    let day = '' + today.getDate();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
+
+  obtenerTiposHabitacion() {
+    this.habitacionService.ListarTiposHabitaciones().subscribe((data: RoomType[]) => {
+      const formTiposHabitaciones = document.getElementById("tipoHabitacion");
+      if (formTiposHabitaciones) {
+        formTiposHabitaciones.innerHTML = '';
+        formTiposHabitaciones.innerHTML += `
+          <option value="" disabled selected>Por favor seleccione el tipo de habitación de su preferencia</option>
+        `;
+        for (let index = 0; index < data.length; index++) {
+          formTiposHabitaciones.innerHTML += `
+            <option value="${data[index].id}">${data[index].name}</option>
+          `;
+        }
+      }
+      this.listaTiposHabitacion = data;
+
+      this.nombreTiposHabitacionMap = {};
+      data.forEach(tipo => {
+        this.nombreTiposHabitacionMap[tipo.id] = tipo.name;
+      });
+
+      this.precioTiposHabitacionMap = {};
+      data.forEach(tipo => {
+        this.precioTiposHabitacionMap[tipo.id] = tipo.price;
+      });
+    });
+  }
+
+  camposValidos(): boolean {
+    return !!this.fechaLlegada && !!this.fechaSalida && !!this.tipoHabitacion;
+  }
+
+  onSubmit() {
+    if (this.camposValidos()) {
+        this.habitacionService.getDisponibilidad(this.fechaLlegada, this.fechaSalida, this.tipoHabitacion)
+            .subscribe(habitaciones => {
+                console.log('Habitaciones recibidas:', habitaciones);
+                this.habitaciones = habitaciones;
+                this.actualizarTabla(); // Llamada a actualizarTabla aquí
+                this.actualizarPaginacion(habitaciones.length);
+                this.tablaVisible = true;
+            }, error => {
+                console.error('Error: ', error);
+                this.mensaje = 'Error al obtener disponibilidad de habitaciones.';
+                this.esError = true;
+                setTimeout(() => {
+                    this.mensaje = '';
+                }, 3000);
+            });
+    } else {
+        this.mensaje = 'Por favor revisa que los campos estén completos.';
+        this.esError = true;
+        setTimeout(() => {
+            this.mensaje = '';
+        }, 3000);
+    }
+}
+  
+
+ actualizarTabla() {
+    console.log('Habitaciones en actualizar tabla',this.habitaciones);
+    const tablaHabitaciones = document.getElementById("table-body");
+    if (tablaHabitaciones) {
+      console.log('Entrando al if (tablaHabitaciones)');
+
+      tablaHabitaciones.innerHTML = '';
+      this.habitaciones.forEach(habitacion => {
+        console.log('Habitaciones en actualizar tabla',this.habitaciones);
+        const nombreTipo = this.nombreTiposHabitacionMap[habitacion.type_id];
+        const precioTipo = this.precioTiposHabitacionMap[habitacion.type_id];
+        console.log('tablaHabitaciones.innerHTML',tablaHabitaciones.innerHTML += `
+        <tr>
+          <td>${habitacion.number}</td>
+          <td>${nombreTipo}</td>
+          <td>${precioTipo}</td>
+        </tr>
+      `);
+        tablaHabitaciones.innerHTML += `
+          <tr>
+            <td>${habitacion.number}</td>
+            <td>${nombreTipo}</td>
+            <td>${precioTipo}</td>
+          </tr>
+        `;
+        console.log('habitacion.number',habitacion.number);
+        console.log('nombreTipo',nombreTipo);
+        console.log('precioTipo',precioTipo);
+
+      });
+      console.log('Saliendo del if (tablaHabitaciones)');
+
+    }
+    console.log('Saliendo del actualizarTabla()', this.nombreTiposHabitacionMap, '---', this.precioTiposHabitacionMap);
+
+  }
+  
+
+  listarHabitaciones() {
+    this.actualizarTabla();
+    this.actualizarPaginacion(this.habitaciones.length);
+  }
+
+  actualizarPaginacion(totalHabitaciones: number) {
+    const totalPaginas = Math.ceil(totalHabitaciones / this.habitacionesPorPagina);
+    const paginacion = document.getElementById("pagination");
+    if (paginacion) {
+      paginacion.innerHTML = "";
+      for (let i = 1; i <= totalPaginas; i++) {
+        const botonPagina = document.createElement("button");
+        botonPagina.classList.add("btn", "btn-secondary", "mr-2");
+        botonPagina.textContent = String(i);
+        botonPagina.addEventListener("click", () => {
+          this.paginaActual = i;
+          this.listarHabitaciones();
+        });
+        paginacion.appendChild(botonPagina);
+      }
+    }
+  }
 }
